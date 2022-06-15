aae46e10b155dc517a18a8fe0b6d17162e8cce2d
torabi <torabi46@gmail.com>
2022-06-15 12:15:55 +0330

    Add pages as children of mainLayout component

--- src/router/index.js
+++ src/router/index.js
@@ -3,13 +3,19 @@ import { createWebHistory, createRouter } from "vue-router";
 const routes = [
   {
     path: "/",
-    name: "Home",
-    component: () => import("../views/Home.vue"),
-  },
-  {
-    path: "/about",
-    name: "About",
-    component: () => import("../views/About.vue"),
+    component: () => import("../components/layouts/MainLayout.vue"),
+    children: [
+      {
+        path: "home",
+        name: "Home",
+        component: () => import("../views/Home.vue"),
+      },
+      {
+        path: "about",
+        name: "About",
+        component: () => import("../views/About.vue"),
+      },
+    ],
   },
 ];
 
